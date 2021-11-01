5526febd0ed7a7ccff5e558de723733feb8b19d2
sarvesh <sarveshmaurya306@gmail.com>
2021-11-02 01:10:39 +0530

    bucket sort

--- 01_TANUJ/index1.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Card from './Card.jsx';
-import './style.css';
-
-ReactDOM.render(
-    <Card />, document.getElementById("root")
-);
