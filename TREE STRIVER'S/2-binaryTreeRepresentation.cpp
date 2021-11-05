4916347fc013598f13584d84e6427f99e7644533
sarvesh <sarveshmaurya306@gmail.com>
2021-11-05 23:55:36 +0530

    finding tree is balanced or not && decode way dp challenge prob memo and dp sol

--- TREE STRIVER'S/2-binaryTreeRepresentation.cpp
+++ TREE STRIVER'S/2-binaryTreeRepresentation.cpp
@@ -21,15 +21,58 @@ void levelOrder(Node<int>* root){
 		if(currentNode->right!=nullptr) q.push(currentNode->right);
 	}
 }
+//same code for height as well
+int maxDepth(Node<int> * root){
+	if(root==nullptr) return 0;
+	int leftDepth= maxDepth(root->left);
+	int rightDepth= maxDepth(root->right);
+	return max(leftDepth, rightDepth)+1;
+}
+
+int isBalanced(Node<int>* root){
+	if(root==nullptr) return 0;
+	int leftDepth= maxDepth(root->left);
+	int rightDepth= maxDepth(root->right);
+	
+	if(leftDepth==-1 || rightDepth==-1) return -1;
+	if(abs(leftDepth-rightDepth)>1) return -1;
+	
+	return max(leftDepth, rightDepth)+1;
+}
+
+//diameter of a tree start here
+int height(Node<int> * root, int &res){
+	if(root==NULL)
+		return 0;
+	
+	int lh= height(root->left, res);
+	int rh= height(root->right, res);
+	res=max(res, lh+rh);
+	return 1+max(lh, rh);
+}
+int diameterOfBinaryTree(Node<int>* root) {
+	int res= INT_MIN;
+	height(root, res);
+	return res;
+}
+//diameter of a tree ends here
+
 
 int main() {
-	Node<int> * root= new Node<int>(1);
+	Node<int> * root= new Node<int>(9);
 	root->left= new Node<int>(2);
 	root->right=new Node<int>(3);
-	root->left->left=new Node<int>(5);
+	root->right->left=new Node<int>(4);
+	root->right->right=new Node<int>(6);
+	root->right->left->left=new Node<int>(5);
+	/*	
+		inorder(root);
+		cout <<endl;
+		levelOrder(root);
+		cout<<endl;
 	
-	inorder(root);
-	cout <<endl;
-	levelOrder(root);
-	cout<<endl;
+		cout<<maxDepth(root)<<endl;
+		isBalanced(root)!=-1?cout<<"Balanced\n":cout<<"Not Balanced\n";
+		cout<<diameterOfBinaryTree(root)<<endl;
+	*/
 }
